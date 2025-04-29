f82672a79651f114d272cbd68322e9e5bbe8b22b
HananDzulHilmi <hanandzulhilmi12@gmail.com>
2025-04-29 16:06:24 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -101,6 +101,34 @@ menu = st.sidebar.selectbox(
     ["🏠 Halaman Utama", "😎 Perkenalan Kelompok", "🔢 Kalkulator Kalori", "📖 Tentang"]
 )
 
+#Mengatur warna latar belakang dan gaya font
+st.markdown(
+    """
+    <style>
+    .stApp {
+        background-color: #f3e9df;  /* Warna latar belakang baru */
+        color: #c99548;             /* Warna teks baru */
+        font-size: 18px;            /* Ukuran font tetap */
+    }
+    .css-1d391kg {
+        color: #c99548;             /* Mengatur warna teks untuk elemen tertentu */
+    }
+    table {
+        border: 2px solid #fb8e54;  /* Garis tabel dengan warna baru */
+    }
+    th {
+        background-color: #fb8e54;  /* Warna latar untuk header tabel */
+        color: white;               /* Warna teks untuk header tabel */
+    }
+    td {
+        background-color: #f3e9df;  /* Warna latar untuk sel tabel */
+        color: black;               /* Warna teks untuk sel tabel */
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
 # Halaman Utama
 if menu == "🏠 Halaman Utama":
     st.title("🍱 Calorie Counting - Aplikasi Gizi Sehatmu")
