935c51415d92f7ba19fe04f509604f1a3994d579
HananDzulHilmi <hanandzulhilmi12@gmail.com>
2025-04-29 16:04:49 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,33 +1,6 @@
 import streamlit as st
 import random
 
-#Mengatur warna latar belakang dan gaya font
-st.markdown(
-    """
-    <style>
-    .stApp {
-        background-color: #f3e9df;  /* Warna latar belakang baru */
-        color: #c99548;             /* Warna teks baru */
-        font-size: 18px;            /* Ukuran font tetap */
-    }
-    .css-1d391kg {
-        color: #c99548;             /* Mengatur warna teks untuk elemen tertentu */
-    }
-    table {
-        border: 2px solid #fb8e54;  /* Garis tabel dengan warna baru */
-    }
-    th {
-        background-color: #fb8e54;  /* Warna latar untuk header tabel */
-        color: white;               /* Warna teks untuk header tabel */
-    }
-    td {
-        background-color: #f3e9df;  /* Warna latar untuk sel tabel */
-        color: black;               /* Warna teks untuk sel tabel */
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
 # Atur halaman
 st.set_page_config(page_title="Calorie Counting", page_icon="🍱", layout="centered")
 
