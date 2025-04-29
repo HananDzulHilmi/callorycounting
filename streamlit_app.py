0e11e56bb7e85207efd032e8f4f716819caac322
HananDzulHilmi <hanandzulhilmi12@gmail.com>
2025-04-29 16:15:39 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -107,7 +107,7 @@ st.markdown(
     <style>
     .stApp {
         background-color: #bab86c;  /* Warna latar belakang baru */
-        color: #c99548;             /* Warna teks baru */
+        color: #FFFFFF;             /* Warna teks baru */
         font-size: 18px;            /* Ukuran font tetap */
     }
     .css-1d391kg {
