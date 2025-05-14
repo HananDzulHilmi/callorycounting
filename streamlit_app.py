f8ff02ec634669609c3736dc1f92bc28f267b548
HananDzulHilmi <hanandzulhilmi12@gmail.com>
2025-05-14 17:34:16 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -149,7 +149,7 @@ elif menu == "🔢 Kalkulator Kalori":
         rekomendasi = buat_menu_4_sehat_5_sempurna(10)
         for i, menu_item in enumerate(rekomendasi, 1):
             st.markdown(f"{i}. {menu_item}")
-        st.snow()
+        st.toast('Mr Stay-Puft')
 
 # Halaman Tentang
 elif menu == "📖 Tentang":
