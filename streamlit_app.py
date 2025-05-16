b7184b5e1400b28193581d7df764e06d01be8197
HananDzulHilmi <hanandzulhilmi12@gmail.com>
2025-05-16 08:39:21 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -96,7 +96,7 @@ if menu == "🏠 Halaman Utama":
 
     Silakan gunakan menu di sebelah kiri untuk mulai 😊
     """)
-    st.error('Error message')
+    st.warning('Warning message')
 # Halaman Perkenalan
 elif menu == "😎 Perkenalan Kelompok":
     st.header("KELOMPOK 6 (E1-PMIP)")
